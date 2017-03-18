692ae3542c45941b6b9048a520d40edffe7121f4
paturishhh <paige.eugenio@gmail.com>
2017-03-19 04:58:50 +0800

    trying out templates and created divs for main page

--- reconwsan/urls.py
+++ reconwsan/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
 from . import views
+
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = [
 	url(r'^$', views.index, name = 'index'),
 	url(r'^test/$', views.test, name = 'test'),
